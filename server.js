b9f4bb2696e01829c3c012c573ca1387b3064c7e
Parassoni <paraskumar4321@gmail.com>
2019-12-20 18:54:43 +0530

    commit

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const path = require("path");
 const cors = require("cors");
 var fs = require("fs");
 app.use(cors());
+const axios = require("axios");
 const port = process.env.PORT || 5000;
 // app.get("/events", (req, res) => {
 //   res.sendFile("./Data.json", { root: __dirname });
 // }
 // console.log(__dirname);
 // +app.get("/events", function(req, res) {
-//   res.sendFile(, { root: __dirname });
+//   res.sendFile("./Data.json", { root: __dirname });
 // });
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use("/", express.static("build"));
 
-  app.get("/events", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "Data.json"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(""));
+  +app.get("/events", function(req, res) {
+    res.sendFile(path.join(__dirname, "", "Data.json"));
   });
 }
-app.listen(port);
+app.listen(port, () => console.log(`listening at ${port}`));
