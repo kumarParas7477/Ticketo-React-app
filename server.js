3916bfffc0f91f9ca81e6e4a6666a65e17de8cb6
Parassoni <paraskumar4321@gmail.com>
2019-12-20 18:14:55 +0530

    commit server

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const port = process.env.PORT || 5000;
 // app.get("/events", (req, res) => {
 //   res.sendFile("./Data.json", { root: __dirname });
 // }
-console.log(__dirname);
-+app.get("/events", function(req, res) {
-  res.sendFile("/Data.json", { root: __dirname });
-});
+// console.log(__dirname);
+// +app.get("/events", function(req, res) {
+//   res.sendFile(, { root: __dirname });
+// });
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use("/", express.static("build"));
+
+  app.get("/events", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "Data.json"));
+  });
+}
 app.listen(port);
